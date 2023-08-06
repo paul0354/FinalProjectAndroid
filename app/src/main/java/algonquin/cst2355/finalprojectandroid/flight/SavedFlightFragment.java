d1af424de31d6e10ed49b557a13362ae82f26ba0
Owen Austin <okowen02@gmail.com>
2023-08-06 04:25:57 -0400

    Austin Commit 10: Can delete from the database, but it isn't appearing that it has been deleted until you re-enter the saved list.

--- app/src/main/java/algonquin/cst2355/finalprojectandroid/flight/SavedFlightFragment.java
+++ app/src/main/java/algonquin/cst2355/finalprojectandroid/flight/SavedFlightFragment.java
@@ -1,4 +1,68 @@
 package algonquin.cst2355.finalprojectandroid.flight;
 
-public class SavedFlightFragment {
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import algonquin.cst2355.finalprojectandroid.databinding.FlightDetailsLayoutBinding;
+import algonquin.cst2355.finalprojectandroid.databinding.SavedFlightsLayoutBinding;
+
+public class SavedFlightFragment extends Fragment {
+
+    FlightInfo selected;
+
+    public SavedFlightFragment(FlightInfo f) {
+        selected = f;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        SavedFlightsLayoutBinding b = SavedFlightsLayoutBinding.inflate(inflater);
+
+        b.savedFlightNum.setText("Flight " + selected.getFlightNum());
+        b.savedDes.setText("Destination airport = " + selected.getDestination());
+        b.savedGate.setText("Gate = " + selected.getGate());
+        b.savedDelay.setText("Delay = " + selected.getDelay());
+        b.savedTerminal.setText("Terminal = " + selected.getTerminal());
+
+        b.deleteButton.setOnClickListener(clk -> {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Would you like to remove this from your saved flights?")
+                    .setTitle("Question:")
+                    .setPositiveButton("Yes", (dialog, cl) -> {
+                        Executor t = Executors.newSingleThreadExecutor();
+                        t.execute(() -> {
+                            FlightActivity.fDAO.deleteFlight(selected);
+                        });
+                        FlightActivity.savedFlights.remove(selected.getId());
+                        SavedFlightsActivity.adapt.notifyItemRemoved((int)selected.getId());
+                        Snackbar.make(b.getRoot(), "Flight removed.", Snackbar.LENGTH_LONG)
+                                .setTextColor(Color.BLACK)
+                                .show();
+                    })
+                    .setNegativeButton("No", (dialog, cl) -> {})
+                    .create().show();
+
+
+        });
+
+        return b.getRoot();
+
+    }
+
 }
--- app/src/main/java/algonquin/cst2355/finalprojectandroid/flight/SavedFlightsActivity.java
+++ app/src/main/java/algonquin/cst2355/finalprojectandroid/flight/SavedFlightsActivity.java
@@ -2,6 +2,8 @@ package algonquin.cst2355.finalprojectandroid.flight;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +12,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import algonquin.cst2355.finalprojectandroid.R;
 import algonquin.cst2355.finalprojectandroid.databinding.ActivitySavedFlightsBinding;
@@ -21,7 +27,8 @@ public class SavedFlightsActivity extends AppCompatActivity {
 
     FlightViewModel flightModel;
 
-    private RecyclerView.Adapter adpt;
+    static protected RecyclerView.Adapter adapt;
+    //ArrayList<FlightInfo> saved = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,18 @@ public class SavedFlightsActivity extends AppCompatActivity {
 
         flightModel = new ViewModelProvider(this).get(FlightViewModel.class);
 
-        binding.savedFlightsRecycler.setAdapter(adpt = new RecyclerView.Adapter<SavedFlightRowHolder>() {
+        FlightActivity.savedFlights = flightModel.listings.getValue();
+
+        if(FlightActivity.savedFlights == null) {
+            flightModel.listings.postValue(FlightActivity.savedFlights = new ArrayList<>());
+            Executor t = Executors.newSingleThreadExecutor();
+            t.execute(() -> {
+                FlightActivity.savedFlights.addAll(FlightActivity.fDAO.getAllFlights());
+                runOnUiThread(() -> binding.savedFlightsRecycler.setAdapter(adapt));
+            });
+        }
+
+        binding.savedFlightsRecycler.setAdapter(adapt = new RecyclerView.Adapter<SavedFlightRowHolder>() {
             @NonNull
             @Override
             public SavedFlightRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -54,7 +72,29 @@ public class SavedFlightsActivity extends AppCompatActivity {
 
         binding.savedFlightsRecycler.setLayoutManager(new LinearLayoutManager(this));
 
+        flightModel.listing.observe(this, (savedFlight) -> {
+
+            SavedFlightFragment frag = new SavedFlightFragment(savedFlight);
+            FragmentManager fMgr = getSupportFragmentManager();
+            FragmentTransaction tx = fMgr.beginTransaction();
+
+            tx.add(R.id.savedFragment, frag)
+                    .replace(R.id.savedFragment, frag)
+                    .addToBackStack(null)
+                    .commit();
+
+//            adapt.notifyDataSetChanged();
+        });
+
     }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        adapt.notifyDataSetChanged();
+//
+//    }
 
     class SavedFlightRowHolder extends RecyclerView.ViewHolder {
 
@@ -72,6 +112,7 @@ public class SavedFlightsActivity extends AppCompatActivity {
 
                 FlightInfo s = FlightActivity.savedFlights.get(pos);
                 flightModel.listing.postValue(s);
+//                adapt.notifyDataSetChanged();
             });
 
         }
