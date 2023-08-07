3ef5d070db439dbc1f99800b12e3a56074724c65
Owen Austin <okowen02@gmail.com>
2023-08-07 14:27:11 -0400

    Austin Commit 15: Fixed the look of flight_list.xml and added comments to FlightActivity.java.

--- app/src/main/java/algonquin/cst2355/finalprojectandroid/flight/FlightActivity.java
+++ app/src/main/java/algonquin/cst2355/finalprojectandroid/flight/FlightActivity.java
@@ -9,13 +9,11 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -53,49 +51,70 @@ TODO: Make it look nice lol
 TODO: Anything I'm missing
  */
 
+/**
+ * This page allows the user to search for flights after entering a source airport code.
+ * The user can save the flights from a fragment, and view them from the toolbar.
+ *
+ * @author Owen Austin
+ * @version 1.0
+ */
 public class FlightActivity extends AppCompatActivity {
+
+    /** For the layout and items in the layout */
     ActivityFlightBinding binding;
+
+    /** Used to save the airport search term for next time */
     SharedPreferences prefs;
+
+    /** Editor for SharedPreferences */
     SharedPreferences.Editor edt;
+
+    /** Adapter used for the RecyclerView, allows refreshing of the RecyclerView */
     private RecyclerView.Adapter adpt;
+
+    /** Access to the flight database */
     static FlightInfoDAO fDAO;
+
+    /** Array list that holds the flights found when searching */
     ArrayList<FlightInfo> flights = new ArrayList<>();
+
+    /** Array list used to hold the saved flights, used in SavedFlightsActivity */
     static ArrayList<FlightInfo> savedFlights = new ArrayList<>();
+
+    /** View model for the flight listings */
     FlightViewModel flightModel;
+
+    /** RequestQueue for the searching of the flights */
     RequestQueue queue = null;
+
+    /** AirportCode entered by the user */
     protected String airportCode;
-    //for logging
-    private static final String TAG = "FlightActivity";
 
-
+    /**
+     * Sets up the layout, onClickListners, JSONRequest, Fragment, database, and SharedPreferences
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityFlightBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //Logging, to check that everything works
-        Log.w(TAG, "In onCreate() - loading widgets");
-
         //sets up internet stuff
         queue = Volley.newRequestQueue(this);
 
         //Sets up the database and view model
         flightModel = new ViewModelProvider(this).get(FlightViewModel.class);
-        //savedFlights = flightModel.listings.getValue();
         FlightDatabase db = Room.databaseBuilder(getApplicationContext(), FlightDatabase.class, "database-name").build();
         fDAO = db.cmDAO();
 
-        //gets saved messages
-        //if(savedFlights == null) {
-            Log.w(TAG, "savedFlights is not null");
-            //flightModel.listings.postValue(savedFlights = new ArrayList<>());
-            savedFlights = new ArrayList<>();
-            Executor thread = Executors.newSingleThreadExecutor();
-            thread.execute(() -> {
-                savedFlights.addAll(fDAO.getAllFlights());
-            });
-        //}
+        //Sets up the saved flights array list
+        savedFlights = new ArrayList<>();
+        Executor thread = Executors.newSingleThreadExecutor();
+        thread.execute(() -> savedFlights.addAll(fDAO.getAllFlights()));
 
         //Sets up shared pref
         prefs = getSharedPreferences("FlightData", Context.MODE_PRIVATE);
@@ -104,28 +123,30 @@ public class FlightActivity extends AppCompatActivity {
         //sets input to what is saved
         binding.inputCode.setText(prefs.getString("FlightData", ""));
 
+        //Sets up the toolbar
         setSupportActionBar(binding.flightToolbar);
 
         //sets what happens when search button is clicked
         binding.searchFlight.setOnClickListener(clk -> {
 
+            //clears what's already on screen
             flights.clear();
             adpt.notifyDataSetChanged();
-
-            Log.w(TAG, "Button has been clicked");
 
             //saves flight code when clicked
             edt.putString("FlightData", binding.inputCode.getText().toString());
             edt.apply();
 
-            airportCode = binding.inputCode.getText().toString(); //gets input
+            //gets the input from user and encodes it
+            airportCode = binding.inputCode.getText().toString();
             String encode = "";
             try {
                 encode = URLEncoder.encode(airportCode, "UTF-8"); //encodes string
             }
             catch(UnsupportedEncodingException e) {e.printStackTrace();}
 
-            if(encode.length() != 3) { //checks if input is an airport code
+            //checks if input is an airport code
+            if(encode.length() != 3) {
 
                 Toast.makeText(this,
                         "Invalid airport code.",
@@ -133,6 +154,7 @@ public class FlightActivity extends AppCompatActivity {
 
             }
             else {
+                //lets the user know that flights are being searched for
                 Toast.makeText(this,
                         "Searching Airport " + binding.inputCode.getText().toString(),
                         Toast.LENGTH_SHORT).show();
@@ -141,15 +163,15 @@ public class FlightActivity extends AppCompatActivity {
                 String url = "http://api.aviationstack.com/v1/flights?access_key="
                         + api + "&dep_iata=" + encode;
 
+                //sends a JSONRequest to avaiationstack
                 JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                         (response) ->{
-
-                    Log.w(TAG, "In JsonObjectRequest() - request went through.");
 
                     try{
 
                         JSONArray data = response.getJSONArray("data");
 
+                        //loops through the JSONRequest, adds flights to the RecyclerView
                         for(int i = 0; i < data.length(); i++) {
 
                             JSONObject dep = data.getJSONObject(i).getJSONObject("departure");
@@ -161,8 +183,6 @@ public class FlightActivity extends AppCompatActivity {
                             String gat = dep.getString("gate");
                             String del = dep.getString("delay");
 
-                            Log.w(TAG, "Flight " + i + "has destination " + des);
-
                             FlightInfo f = new FlightInfo(fN, des, ter, gat, del);
                             flights.add(f);
                             adpt.notifyItemInserted(flights.size()-1);
@@ -173,14 +193,12 @@ public class FlightActivity extends AppCompatActivity {
                         throw new RuntimeException(e);
                     }
 
-                        }, (error) -> {int i = 0;});
+                        }, (error) -> {});
                 queue.add(request);
             }
         });
 
-        //gets the airport code from shared prefs
-
-        //for recyclerview
+        //Sets the adapter up for the RecyclerView
         binding.flights.setAdapter(adpt = new RecyclerView.Adapter<FlightRowHolder>() {
             @NonNull
             @Override
@@ -207,10 +225,8 @@ public class FlightActivity extends AppCompatActivity {
         //for recyclerview
         binding.flights.setLayoutManager(new LinearLayoutManager(this));
 
-        //for the fragment view
+        //When an item is clicked in the recyclerview, creates a fragment with more info
         flightModel.listing.observe(this, (newFlightValue) -> {
-
-            int size = savedFlights.size();
 
             FlightDetailsFragment flightFragment = new FlightDetailsFragment(newFlightValue);
             FragmentManager fMgr = getSupportFragmentManager();
@@ -224,6 +240,9 @@ public class FlightActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Saves the input from the user when this activity is paused
+     */
     @Override
     protected void onPause() { //for saving airport code
 
@@ -233,6 +252,13 @@ public class FlightActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Creates the options menu from a given menu
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -240,16 +266,23 @@ public class FlightActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Gives functionality to each of the items in the menu
+     *
+     * @param item The menu item that was selected.
+     *
+     * @return true
+     */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if(item.getItemId()==R.id.savedFlights) {
+        if(item.getItemId()==R.id.savedFlights) { //Goes to the saved flights activity
 
             Intent saved = new Intent(FlightActivity.this, SavedFlightsActivity.class);
             startActivity(saved);
 
         }
-        else if(item.getItemId()==R.id.flightHelp) {
+        else if(item.getItemId()==R.id.flightHelp) { //Opens a help dialog
 
             AlertDialog.Builder builder = new AlertDialog.Builder(FlightActivity.this);
             builder.setMessage("To use this app, type an airport code into the searchbar. " +
@@ -266,19 +299,19 @@ public class FlightActivity extends AppCompatActivity {
                     .create().show();
 
         }
-        else if(item.getItemId()==R.id.flightToBear) {
+        else if(item.getItemId()==R.id.flightToBear) { //Goes to the Bear Activity
 
             Intent bear = new Intent(FlightActivity.this, BearActivity.class);
             startActivity(bear);
 
         }
-        else if(item.getItemId()==R.id.flightToCurrency) {
+        else if(item.getItemId()==R.id.flightToCurrency) { //Goes to the Currency Activity
 
             Intent currency = new Intent(FlightActivity.this, CurrencyActivity.class);
             startActivity(currency);
 
         }
-        else if(item.getItemId()==R.id.flightToTrivia) {
+        else if(item.getItemId()==R.id.flightToTrivia) { //Goes to the Trivia Activity
 
             Intent trivia = new Intent(FlightActivity.this, TriviaActivity.class);
             startActivity(trivia);
@@ -289,14 +322,26 @@ public class FlightActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * This class gives functionality to  the elements in the recyclerview
+     */
     class FlightRowHolder extends RecyclerView.ViewHolder {
 
+        /** Where the flight number goes in the recyclerview */
         TextView flightNum;
+
+        /** Where the destination code goes in the recyclerview */
         TextView destCode;
 
+        /**
+         * Adds on click functionality to the elements in a recyclerview
+         *
+         * @param itemView the specific element in the recyclerview
+         */
         public FlightRowHolder(@NonNull View itemView) {
             super(itemView);
 
+            //pairs the flightNum and destCode to the ids of the recyclerview
             flightNum = itemView.findViewById(R.id.flightNumber);
             destCode = itemView.findViewById(R.id.destination);
 
